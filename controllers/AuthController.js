63ba915231dcc6a76afcb9b2eb723641b13e114e
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-12-08 18:58:18 -0500

    auth controller and routes working

---
+++ controllers/AuthController.js
@@ -0,0 +1,95 @@
+const { User } = require('../models')
+const middleware = require('../middleware')
+
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { userName: req.body.userName },
+      raw: true
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(user.passwordDigest, req.body.password))
+    ) {
+      let payload = {
+        id: user.id,
+        userName: user.userName
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized User' })
+  } catch (error) {
+    throw error
+  }
+}
+
+const Register = async (req, res) => {
+  try {
+    const { userName, password, profileImage } = req.body
+    let passwordDigest = await middleware.hashPassword(password)
+    const user = await User.create({ userName, passwordDigest, profileImage })
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
+// const updatePassword = async (req, res) => {
+//   try {
+//     const user = await User.findOne({
+//       where: { userName: req.body.userName },
+//       raw: true
+//     })
+//     const { password } = req.body
+//     let passwordDigest = await middleware.hashPassword(password)
+//     if (user && passwordDigest !== user.passwordDigest) {
+//       const update = await User.update(
+//         { passwordDigest },
+//         {
+//           where: {
+//             passwordDigest: user.passwordDigest
+//           }
+//         }
+//       )
+//       res.send(update)
+//     }
+//     res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+//   } catch (error) {
+//     throw error
+//   }
+// }
+
+const updatePassword = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { userName: req.body.userName }
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(
+        user.dataValues.passwordDigest,
+        req.body.oldPassword
+      ))
+    ) {
+      let passwordDigest = await middleware.hashPassword(req.body.newPassword)
+      await user.update({ passwordDigest })
+      return res.send({ status: 'Success', msg: 'Password updated' })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    throw error
+  }
+}
+
+const CheckSession = async (req, res) => {
+  const { payload } = res.locals
+  res.send(payload)
+}
+
+module.exports = {
+  Login,
+  Register,
+  updatePassword,
+  CheckSession
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
+const AuthRouter = require('./routes/AuthRouter')
 
 const app = express()
 
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 3001
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
+
+//routes
+app.use('/auth', AuthRouter)
 
 app.get('/', (req, res) => res.json({ message: 'server worksss' }))
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
